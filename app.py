ede9db6cb4b75f726c3d2b4eeed4ad77572f1b5f
root <root@v20555.luxvps.net>
2024-07-13 07:07:12 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, File, UploadFile
+import subprocess as sp
+from pydantic import BaseModel
+from typing import Optional
+import shutil
+import uuid
+import os
+
+app = FastAPI()
+
+class ClamAV(BaseModel):
+    infected: Optional[bool] = None
+    result: Optional[str] = ""
+    engine: Optional[str] = ""
+    known: Optional[str] = ""
+    updated: Optional[str] = ""
+    error: Optional[str] = ""
+
+def sanitize_filename(filename: str) -> str:
+    # Replace spaces with underscores
+    return filename.replace(" ", "_")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/scan", response_model=ClamAV)
+async def scan(malware: UploadFile = File(...)) -> ClamAV:
+    try:
+        clamav = ClamAV()
+        sanitized_filename = sanitize_filename(malware.filename)
+        file_location = f"/malware/{uuid.uuid4()}-{sanitized_filename}"
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(malware.file, buffer)
+        output = sp.getoutput(f"clamscan {file_location}")
+        
+        result = output
+        result_line_arr = result.split('\n')
+        virus_result = result_line_arr[0].split(": ")[1].replace(" FOUND", "") or ""
+        clamav.result = virus_result
+        clamav.infected = "FOUND" in result_line_arr[0]
+        for line in result.splitlines():
+            if "Known viruses:" in line:
+                clamav.known = line.split(": ")[1]
+            if "Engine version:" in line:
+                clamav.engine = line.split(": ")[1]
+        os.remove(file_location)
+        return clamav
+    except Exception as e:
+        print(e)
+        return {"error": str(e)}, 500
+
+@app.get("/update") 
+async def update():
+    try:
+        output = sp.getoutput('freshclam')
+        print(output)
+        return {"status": 200, "message": output}
+    except Exception as e:
+        return {"status": 500, "message": str(e)}
+    return {"status": 200}
